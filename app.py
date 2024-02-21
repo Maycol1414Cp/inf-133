864be0c30f898d852c6759628bd801c7fb5752c1
Maycol1414Cp <maycol1414cp@gmail.com>
2024-02-21 19:44:03 -0400

    issue app.py fixed

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-def run(server_addres=HTTPServer, handler_class=SimpleHTTPRequestHandler):
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
     try:
         server_addres=('',8000)
         httpd=server_class(server_addres,handler_class)
         print("inciando servidor web en http://localhost:8000/")
-        httpd.server_forever()
+        httpd.serve_forever()
     except KeyboardInterrupt:
         print('apagando el servidor')
         httpd.socket.close()
-if _name_ == "_main_":
-    run()    
+if __name__ == "__main__":
+    run()
